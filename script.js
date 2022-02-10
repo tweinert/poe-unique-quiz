945d9fb952a2b9d58f7d191c24199ad9964ecc1b
tweinert <tait.weinert@outlook.com>
2022-02-10 17:59:45 +1030

    random item from json

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 const nameText = document.getElementById("nameText");
 const testText = document.getElementById("testText");
 
+let dataObj = {};
+
+function getJson() {
+    fetch('./test.json')
+        .then(response => response.json())
+        .then(data => {
+            // data is object containing full csv text
+            // gets keys from json file
+            const itemKeys = Object.keys(data);
+            
+            // gets random index and key
+            const randIndex = Math.floor(Math.random() * itemKeys.length);
+
+            // random item name
+            const randItemKey = itemKeys[randIndex];
+
+            // item url matching randKey
+            const itemUrl = data[randItemKey];
+            console.log(randItemKey + " " + itemUrl);
+        })
+        .catch(err => console.log(err));
+}
+
+getJson();
